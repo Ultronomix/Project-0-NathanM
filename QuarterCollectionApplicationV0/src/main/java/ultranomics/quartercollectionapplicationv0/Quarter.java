b88312a71e251d963f202db3435072dec4f23272
NMooney <nmooney96@hotmail.com>
2022-08-22 23:55:32 -0400

    State and Mint ENUMs
    Added ENUM Variables for State and Mint IDs

--- QuarterCollectionApplicationV0/src/main/java/ultranomics/quartercollectionapplicationv0/Quarter.java
+++ QuarterCollectionApplicationV0/src/main/java/ultranomics/quartercollectionapplicationv0/Quarter.java
@@ -3,7 +3,67 @@ package ultranomics.quartercollectionapplicationv0;
 public class Quarter{
     short idNumber;
     short idYear;
-    byte idState;
-    byte idMint; 
+    State idState;
+    Mint idMint; 
     boolean inCollection;
+}
+
+enum State{
+    ALABAMA,
+    ALASKA,
+    ARIZONA,
+    ARKANSAS,
+    CALIFORNIA,
+    COLORADO,
+    CONNECTICUT,
+    DELAWARE,
+    FLORIDA,
+    GEORGIA,
+    HAWAII,
+    IDAHO,
+    ILLINOIS,
+    INDIANA,
+    IOWA,
+    KANSAS,
+    KENTUCKY,
+    LOUISIANA,
+    MAINE,
+    MARYLAND,
+    MASSACHUSETTS,
+    MICHIGAN,
+    MINNESOTA,
+    MISSISSIPPI,
+    MISSOURI,
+    MONTANA,
+    NEBRASKA,
+    NEVADA,
+    NEW_HAMPSHIRE,
+    NEW_JERSEY,
+    NEW_MEXICO,
+    NEW_YORK,
+    NORTH_CAROLINA,
+    NORTH_DAKOTA,
+    OHIO,
+    OKLAHOMA,
+    OREGON,
+    PENNSYLVANIA,
+    RHODE_ISLAND,
+    SOUTH_CAROLINA,
+    SOUTH_DAKOTA,
+    TENNESSEE,
+    TEXAS,
+    UTAH,
+    VERMONT,
+    VIRGINIA,
+    WASHINGTON,
+    WEST_VIRGINIA,
+    WISCONSIN,
+    WYOMING
+
+}
+
+enum Mint{
+    PHILADELPHIA,
+    DENVER,
+    WESTPOINT
 }
