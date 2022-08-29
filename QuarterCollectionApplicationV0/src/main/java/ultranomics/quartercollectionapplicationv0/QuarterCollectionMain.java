725b2f8583c8d9639ef68772aec1288287a67123
NMooney <nmooney96@hotmail.com>
2022-08-29 19:12:58 -0400

    ConnectionFactory Class
    Created and began implementation of ConnectionFactory class following lecture example

--- QuarterCollectionApplicationV0/src/main/java/ultranomics/quartercollectionapplicationv0/QuarterCollectionMain.java
+++ QuarterCollectionApplicationV0/src/main/java/ultranomics/quartercollectionapplicationv0/QuarterCollectionMain.java
@@ -1,37 +1,19 @@
 package ultranomics.quartercollectionapplicationv0;
 
-
-import java.io.FileReader;
-import java.io.IOException;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
+import ultranomics.quartercollectionapplicationv0.common.datasource.ConnectionFactory;
 
 public class QuarterCollectionMain {
     public static void main (String[] args){
-        
-        
+        ConnectionFactory connection = new ConnectionFactory();
         try{
-            Properties dbprops = new Properties();
-            dbprops.load(new FileReader("src/main/resources/application.properties"));
-            
-            Class.forName("org.postgresql.Driver");
-            Connection conn = DriverManager.getConnection(dbprops.getProperty("db-url"), dbprops.getProperty("db-username"), dbprops.getProperty("db-password"));
+            Connection conn = connection.getConnection();
             if(conn != null){
-                System.out.println("Connection established");
+                System.out.println("Connection Established");
             }
+        }catch(SQLException e){
+            System.err.println("Error: could not establish connection to Database");
         }
-        catch(IOException e){
-            System.err.println("Error: could not read from application file");
-        }
-        catch(SQLException e){
-            e.printStackTrace();
-            System.err.println("Error: connection to database could not be established");
-        }
-        catch(ClassNotFoundException e){
-            System.err.println("Error: PostgreSQL  JDBC Driver failed to load");
-        }
-        
     }//end main method
 }//end class
