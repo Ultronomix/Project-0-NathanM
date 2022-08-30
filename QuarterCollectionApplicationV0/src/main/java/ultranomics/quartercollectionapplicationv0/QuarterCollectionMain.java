c2dadb0ef66ba686ce0d62a255280f51d1801385
NMooney <nmooney96@hotmail.com>
2022-08-30 13:55:57 -0400

    Reworked Main
    Main now starts the Menu process

--- QuarterCollectionApplicationV0/src/main/java/ultranomics/quartercollectionapplicationv0/QuarterCollectionMain.java
+++ QuarterCollectionApplicationV0/src/main/java/ultranomics/quartercollectionapplicationv0/QuarterCollectionMain.java
@@ -3,18 +3,13 @@ package ultranomics.quartercollectionapplicationv0;
 import java.sql.Connection;
 import java.sql.SQLException;
 import ultranomics.quartercollectionapplicationv0.common.datasource.ConnectionFactory;
+import ultranomics.quartercollectionapplicationv0.common.screens.WelcomeMenu;
 
 public class QuarterCollectionMain {
     public static void main (String[] args){
-        ConnectionFactory connection = new ConnectionFactory();
-        try{
-            Connection conn = connection.getConnection();
-            if(conn != null){
-                System.out.println("Connection Established");
-            }
-        }catch(SQLException e){
-            System.err.println("Error: could not establish connection to Database");
-            System.exit(0);
-        }
+        
+        WelcomeMenu.menu();
+        
+        
     }//end main method
 }//end class
